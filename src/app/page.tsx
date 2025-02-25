580dec0da712bd5bc4f8fd7d9cc484f801797354
Winn Z <winnwyz@gmail.com>
2025-02-25 15:50:10 +1100

    Set up authentication using clerk

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,18 @@
+import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
+
 export default function Home() {
   return (
     <>
-      home
+      <SignedOut>
+        <SignInButton mode="modal">
+          <button className="bg-red-500">
+            Sign in
+          </button>
+        </SignInButton>
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </>
-  )
+  );
 }
