f1a5697b7e8a23e3a16a8249007f7f33ac119437
winnie <winnwyz@gmail.com>
2025-02-27 15:51:49 +1100

    Add user to database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,3 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/nextjs";
-import { Button } from "@/components/ui/button";
-import ModeToggle from "@/components/ModeToggle";
-
 export default function Home() {
   return (
     <>
