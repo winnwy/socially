c312ac7a5510348b591366ea81871b7f0c0e985c
winnie <winnwyz@gmail.com>
2025-03-06 16:21:25 +1100

    Fix linting errors with the nextjs15 pattern

--- src/app/profile/[username]/page.tsx
+++ src/app/profile/[username]/page.tsx
@@ -7,8 +7,9 @@ import {
   import { notFound } from "next/navigation";
   import ProfilePageClient from "./ProfilePageClient"
   
-  export async function generateMetadata({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
+  export async function generateMetadata({ params }: { params: Promise<{ username: string }> }) {
+    const { username } = await params;
+    const user = await getProfileByUsername(username);
     if (!user) return;
   
     return {
@@ -17,8 +18,9 @@ import {
     };
   }
   
-  async function ProfilePageServer({ params }: { params: { username: string } }) {
-    const user = await getProfileByUsername(params.username);
+  async function ProfilePageServer({ params }: { params:  Promise<{ username: string }> }) {
+    const {username} = await params;
+    const user = await getProfileByUsername(username);
   
     if (!user) notFound();
   
